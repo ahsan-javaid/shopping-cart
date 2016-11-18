b2130b851b7b2289848d82c4641a406578a6d63e
Ahsan Javaid <ahsan.javaid@novatoresols.com>
2016-11-18 15:48:46 +0500

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var port = process.env.PORT || 8080; // set our port
-mongoose.connect("mongodb://localhost/productDb");
+
+//mongoose.connect("mongodb://localhost/productDb");
+mongoose.connect("mongodb://root:root@ds157487.mlab.com:57487/product-db");
+
+
+
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
